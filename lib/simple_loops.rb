82f85f41e048ec5ea84cff3c681304f8aa9edfe7
Donat Pllana <donat.pllana@outlook.com>
2020-06-05 18:29:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,16 @@
 # Write your methods here
 def loop_message_five_times(message)
-  counter=0
-  while counter<5 do
+  counter=1
+  while counter<=5 do
     puts message
     counter+=1
   end
+end
+def loop_message_n_times(message,number)
+  counter=1
+  while counter<=number do
+    puts message
+    counter+=1
+  end
+end
+  
