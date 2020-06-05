a227068f1ac97d884ca5541e249206c073e9cbac
Donat Pllana <donat.pllana@outlook.com>
2020-06-05 18:38:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,3 +20,9 @@ def output_array(array)
     counter+=1
   end
 end
+def return_string_array(array)
+  counter=0
+  while counter<array.length do
+    array[counter]=array.counter.to_s
+    counter+=1
+  end
